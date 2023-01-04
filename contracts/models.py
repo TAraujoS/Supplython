c6673d2d60fc1f6f2d0379382ae83f0be99ece11
João Victor Lemos <jolita.0312@gmail.com>
2023-01-04 15:15:59 -0300

    feat: relacionamentos de contract

--- contracts/models.py
+++ contracts/models.py
@@ -4,3 +4,16 @@ from django.db import models
 class Contract(models.Model):
     duration = models.DateField(required=True)
     value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    # N:1 -> category -> contract
+    category = models.ForeignKey(
+        "categories.Category",
+        on_delete=models.CASCADE,
+        related_name="contracts",
+    )
+    # N:1 ->  suppliers -> contract
+    supplier = models.ForeignKey(
+        "suppliers.Supplier",
+        on_delete=models.CASCADE,
+        related_name="contracts",
+    )
