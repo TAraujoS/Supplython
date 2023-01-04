8a4e1c6c662724e3073ab49182b16ae04b34aff8
Ricardo Martins <102545943+ORMartins02@users.noreply.github.com>
2023-01-04 14:46:04 -0300

    feat: relations invoice-contract

--- invoices/models.py
+++ invoices/models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 
 class Invoice(models.Model):
-    class Meta:
-        ordering = ["id"]
-
     invoice_number = models.CharField(max_length=10)
     value = models.DecimalField(max_digits=8, decimal_places=2)
     description = models.CharField(max_length=140)
     verified = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     validity = models.DateField()
+
+    contract = models.ForeignKey(
+        "contracts.Contract",
+        on_delete=models.CASCADE,
+        related_name="invoices",
+    )
+
+    supplier = models.ForeignKey(
+        "suppliers.Supplier",
+        on_delete=models.CASCADE,
+        related_name="invoices",
+    )
+
+    employee = models.ForeignKey(
+        "employees.Employee",
+        on_delete=models.CASCADE,
+        related_name="invoices",
+    )
+
