c1bc96f556387f69a58401082dfb35cc6583c14f
João Victor Lemos <jolita.0312@gmail.com>
2023-01-05 10:54:43 -0300

    Feat: Relations of contract

--- contracts/views.py
+++ contracts/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Contract
+from categories.models import Category
+from suppliers.models import Supplier
 from .serializers import ContractSerializer
 from employees.permissions import IsManager
+from django.shortcuts import get_object_or_404
 
 
 class ContractView(generics.ListCreateAPIView):
@@ -13,6 +16,11 @@ class ContractView(generics.ListCreateAPIView):
     queryset = Contract.objects.all()
 
 
+def perform_create(self, serializer):
+    categories = get_object_or_404(Category, pk=self.kwargs["pk"])
+    return serializer.save(categories=categories)
+
+
 class ContractDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     authentication_classes = [JWTAuthentication]
@@ -20,3 +28,7 @@ class ContractDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     serializer_class = ContractSerializer
     queryset = Contract.objects.all()
+
+    def perform_create(self, serializer):
+        supplier = get_object_or_404(Supplier, pk=self.kwargs["pk"])
+        return serializer.save(supplier=supplier)
