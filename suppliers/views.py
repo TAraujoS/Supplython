e4accf8d4005d9c27cd7f211a3586bbeb2483d9a
larissa-sato <larissasato_@outlook.com>
2023-01-06 14:36:43 -0300

    finished feat/relations-supplier

--- suppliers/views.py
+++ suppliers/views.py
@@ -3,7 +3,21 @@ from .serializers import SupplierSerializer, SupplierDetailSerializer
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from employees.permissions import IsManager
+from drf_spectacular.utils import extend_schema_view, extend_schema
 
+
+@extend_schema_view(
+    post=extend_schema(
+        description="Route to create suppliers",
+        summary="Create Supplier",
+        tags=["Suppliers"],
+    ),
+    get=extend_schema(
+        description="Route for a manager to list all suppliers",
+        summary="List all suppliers",
+        tags=["Suppliers"],
+    ),
+)
 
 class SupplierView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -16,6 +30,21 @@ class SupplierView(generics.ListCreateAPIView):
         return SupplierSerializer
 
     queryset = Supplier.objects.all()
+
+
+
+@extend_schema_view(
+    get=extend_schema(
+        description="Route for a manager to list a single supplier",
+        summary="List supplier",
+        tags=["Suppliers"],
+    ),
+    patch=extend_schema(
+        description="Route for a manager to update a supplier",
+        summary="Update supplier",
+        tags=["Suppliers"],
+    ),
+)
 
     
 class SupplierDetailView(generics.RetrieveUpdateDestroyAPIView):
