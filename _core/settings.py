13da8aaecfa73b0710c9f103b29e1bdc0f9c8fbe
Thiago AS <thiago-pin@gotmail.com>
2023-01-12 00:19:31 -0300

    feat: docker database active

--- _core/settings.py
+++ _core/settings.py
@@ -99,15 +99,15 @@ WSGI_APPLICATION = "_core.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.getenv("POSTGRES_DB"),
-    #     "USER": os.getenv("POSTGRES_USER"),
-    #     "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-    #     "HOST": "supplython_db",
-    #     "PORT": "5432",
-    # },
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "supplython_db",
+        "PORT": "5432",
+    },
+    "dbsqlite3": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
